1dd000b86179352df699b8785207c0e23e3a32dd
zat-is-me <taahm1@morgan.edu>
2021-01-20 18:02:49 -0600

    added ArrayDriver file

---
+++ ArrayDriver.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Shape_1 = require("./Shape");
+var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
+var myShape = new Shape_1.Shape(10, 15);
+var myCircle = new Circle_1.Circle(5, 10, 20);
+var myRectangle = new Rectangle_1.Rectangle(0, 0, 3, 7);
+var theShapes = [];
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+for (var _i = 0, theShapes_1 = theShapes; _i < theShapes_1.length; _i++) {
+    var iterator = theShapes_1[_i];
+    console.log(iterator.getInfo());
+}
